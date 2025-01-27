70a9ecaf781c6103c181f09c56f0ebd6ac142eed
kouhei <kouhei.basketball7.8@gmail.com>
2025-01-27 18:26:13 +0900

    add:ダミーデータ作成

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -12,6 +14,28 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $products = [
+            [ 'name' => '黒ボールペン','code'=>'P001','price'=>201,'tax'=>10 ],
+            [ 'name' => '赤ボールペン','code'=>'P002','price'=>202,'tax'=>10 ],
+            [ 'name' => '青ボールペン','code'=>'P003','price'=>203,'tax'=>10 ],
+            [ 'name' => '緑ボールペン','code'=>'P004','price'=>204,'tax'=>10 ],
+            [ 'name' => '紫ボールペン','code'=>'P005','price'=>205,'tax'=>10 ],
+            [ 'name' => '黒シャープペン','code'=>'P006','price'=>206,'tax'=>10 ],
+            [ 'name' => '赤シャープペン','code'=>'P007','price'=>207,'tax'=>10 ],
+            [ 'name' => '青シャープペン','code'=>'P008','price'=>208,'tax'=>10 ],
+            [ 'name' => '緑シャープペン','code'=>'P009','price'=>209,'tax'=>10 ],
+            [ 'name' => '紫シャープペン','code'=>'P010','price'=>210,'tax'=>10 ],
+            [ 'name' => 'メロンパン','code'=>'F001','price'=>180,'tax'=>8 ],
+            [ 'name' => 'アンパン','code'=>'F002','price'=>200,'tax'=>8 ],
+        ];
+
+        foreach($products as $product) {
+            DB::table('products')->insert([
+                'product_name' => $product['name'],
+                'product_code' => $product['code'],
+                'product_price' => $product['price'],
+                'product_tax' => $product['tax']
+            ]);
+        }
     }
 }
