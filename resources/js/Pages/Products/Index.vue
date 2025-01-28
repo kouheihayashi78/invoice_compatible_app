98f6920846b79c70c7d87ae7f224a72e0005a73f
kouhei <kouhei.basketball7.8@gmail.com>
2025-01-28 17:05:36 +0900

    add:作成したvueファイルを表示できるように実装

---
+++ resources/js/Pages/Products/Index.vue
@@ -0,0 +1,34 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Products" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Products</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">商品一覧</div>
+                </div>
+            </div>
+
+            <div class="m-3 max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-2">
+                    <div class="mt-3 mb-3 ml-3 flex">
+                        <!-- Linkタグを使うことでaタグと違ってページ全体を再読み込みせずにリンクを処理できる -->
+                        <Link :href="route('product.create')" :class="'px-4 py-2 bg-indigo-500 text-white border rounded-md font-semibold text-xs'" >
+                            <i class="fa-solid fa-plus-circle"></i> 商品登録
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
