2532a855357cd7f690d81e1840f67d1b33b3ebe9
kouhei <kouhei.basketball7.8@gmail.com>
2025-02-10 14:16:05 +0900

    add: オーダー登録ページで選択した商品情報表示、登録したい個数を入力するinput欄を実装

--- resources/js/Pages/Orders/Create.vue
+++ resources/js/Pages/Orders/Create.vue
@@ -6,6 +6,8 @@ import PrimaryButton from "@/Components/PrimaryButton.vue";
 import SelectInput from "@/Components/SelectInput.vue";
 import { Head, Link, useForm } from "@inertiajs/vue3";
 import NavLink from "@/Components/NavLink.vue";
+import { computed } from "vue";
+import TextInput from "@/Components/TextInput.vue";
 
 const props = defineProps({
     products: {type:Object},
@@ -22,10 +24,21 @@ const form = useForm({
     num3 : '',
 });
 
+// 選択した商品の情報を取得する
+const selectedProduct1 = computed(() => {
+    return props.products.data.find((product) => product.id == form.product_id1)
+});
+const selectedProduct2 = computed(() => {
+    return props.products.data.find((product) => product.id == form.product_id2)
+});
+const selectedProduct3 = computed(() => {
+    return props.products.data.find((product) => product.id == form.product_id3)
+});
+
 const submit = () => {
     form.post(route("order.store"), {
         // onFinishだとバリデーション発生時もリセットされてしまうのでOnSuccess
-        onSuccess: () => form.reset("customer_id", "product_id1", "product_id2", "product_id3"),
+        onSuccess: () => form.reset("customer_id", "product_id1", "product_id2", "product_id3", "num1", "num2", "num3"),
     });
 };
 </script>
@@ -56,9 +69,10 @@ const submit = () => {
                                 :options="customers.data"
                                 id="customer_id"
                                 type="text"
-                                class="mt-1 block w-full"
+                                class="mt-1 block w-80"
                                 v-model="form.customer_id"
                                 required
+                                autofocus
                                 autocomplete="name"
                             />
 
@@ -68,18 +82,58 @@ const submit = () => {
                             />
                         </div>
 
-                        <div class="mt-4">
-                            <InputLabel for="product_id1" value="商品1" />
+                        <div class="mt-4 flex">
+                            <div>
+                                <InputLabel for="product_id1" value="商品1" />
 
-                            <SelectInput
-                                :options="products.data"
-                                id="product_id1"
-                                type="text"
-                                class="mt-1 block w-full"
-                                v-model="form.product_id1"
-                                required
-                                autocomplete="name"
-                            />
+                                <SelectInput
+                                    :options="products.data"
+                                    id="product_id1"
+                                    type="text"
+                                    class="mt-1 block w-80"
+                                    v-model="form.product_id1"
+                                    required
+                                    autocomplete="name"
+                                />
+                            </div>
+
+                            <!-- 商品1が選択されたら、商品の情報表示、個数を入力するフォームを表示 -->
+                            <div v-if="selectedProduct1" class="flex">
+                                <div class="ml-10 w-24">
+                                    <InputLabel value="コード"/>
+                                    <div class="mt-3 ml-2 font-medium text-xl">
+                                        {{ selectedProduct1.code }}
+                                    </div>
+                                </div>
+                                <div class="ml-10 w-24">
+                                    <InputLabel value="価格"/>
+                                    <div class="mt-3 ml-2 font-medium text-xl">
+                                        {{ selectedProduct1.price }}円
+                                    </div>
+                                </div>
+                                <div class="ml-10 w-24">
+                                    <InputLabel value="税率"/>
+                                    <div class="mt-3 ml-2 font-medium text-xl">
+                                        {{ selectedProduct1.tax }}%
+                                    </div>
+                                </div>
+                                <!-- 注文数を数字で入力し、データを送信できる様にする -->
+                                <div class="ml-10 w-24">
+                                    <InputLabel for="num1" value="注文数"/>
+                                    <TextInput
+                                        v-model="form.num1"
+                                        id="num1"
+                                        type="number"
+                                        class="w-full"
+                                        autocomplete="num1"
+                                        required
+                                    />
+                                </div>
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.num1"
+                                />
+                            </div>
 
                             <InputError
                                 class="mt-2"
@@ -87,18 +141,56 @@ const submit = () => {
                             />
                         </div>
 
-                        <div class="mt-4">
-                            <InputLabel for="product_id2" value="商品2" />
+                        <div class="mt-4 flex">
+                            <div>
+                                <InputLabel for="product_id2" value="商品2" />
 
-                            <SelectInput
-                                :options="products.data"
-                                id="product_id2"
-                                type="text"
-                                class="mt-1 block w-full"
-                                v-model="form.product_id2"
-                                required
-                                autocomplete="name"
-                            />
+                                <SelectInput
+                                    :options="products.data"
+                                    id="product_id2"
+                                    type="text"
+                                    class="mt-1 block w-80"
+                                    v-model="form.product_id2"
+                                    autocomplete="name"
+                                />
+                            </div>
+
+                            <div v-if="selectedProduct2" class="flex">
+                                <div class="ml-10 w-24">
+                                    <InputLabel value="コード"/>
+                                    <div class="mt-3 ml-2 font-medium text-xl">
+                                        {{ selectedProduct2.code }}
+                                    </div>
+                                </div>
+                                <div class="ml-10 w-24">
+                                    <InputLabel value="価格"/>
+                                    <div class="mt-3 ml-2 font-medium text-xl">
+                                        {{ selectedProduct2.price }}円
+                                    </div>
+                                </div>
+                                <div class="ml-10 w-24">
+                                    <InputLabel value="税率"/>
+                                    <div class="mt-3 ml-2 font-medium text-xl">
+                                        {{ selectedProduct2.tax }}%
+                                    </div>
+                                </div>
+
+                                <div class="ml-10 w-24">
+                                    <InputLabel for="num2" value="注文数"/>
+                                    <TextInput
+                                        v-model="form.num2"
+                                        id="num2"
+                                        type="number"
+                                        class="w-full"
+                                        autocomplete="num2"
+                                        required
+                                    />
+                                </div>
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.num2"
+                                />
+                            </div>
 
                             <InputError
                                 class="mt-2"
@@ -106,18 +198,56 @@ const submit = () => {
                             />
                         </div>
 
-                        <div class="mt-4">
-                            <InputLabel for="product_id3" value="商品3" />
+                        <div class="mt-4 flex">
+                            <div>
+                                <InputLabel for="product_id3" value="商品3" />
 
-                            <SelectInput
-                                :options="products.data"
-                                id="product_id3"
-                                type="text"
-                                class="mt-1 block w-full"
-                                v-model="form.product_id3"
-                                required
-                                autocomplete="name"
-                            />
+                                <SelectInput
+                                    :options="products.data"
+                                    id="product_id3"
+                                    type="text"
+                                    class="mt-1 block w-80"
+                                    v-model="form.product_id3"
+                                    autocomplete="name"
+                                />
+                            </div>
+
+                            <div v-if="selectedProduct3" class="flex">
+                                <div class="ml-10 w-24">
+                                    <InputLabel value="コード"/>
+                                    <div class="mt-3 ml-2 font-medium text-xl">
+                                        {{ selectedProduct3.code }}
+                                    </div>
+                                </div>
+                                <div class="ml-10 w-24">
+                                    <InputLabel value="価格"/>
+                                    <div class="mt-3 ml-2 font-medium text-xl">
+                                        {{ selectedProduct3.price }}円
+                                    </div>
+                                </div>
+                                <div class="ml-10 w-24">
+                                    <InputLabel value="税率"/>
+                                    <div class="mt-3 ml-2 font-medium text-xl">
+                                        {{ selectedProduct3.tax }}%
+                                    </div>
+                                </div>
+
+                                <div class="ml-10 w-24">
+                                    <InputLabel for="num3" value="注文数"/>
+                                    <TextInput
+                                        v-model="form.num3"
+                                        id="num3"
+                                        type="number"
+                                        class="w-full"
+                                        autocomplete="num3"
+                                        required
+                                    />
+                                </div>
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.num3"
+                                />
+                            </div>
 
                             <InputError
                                 class="mt-2"
